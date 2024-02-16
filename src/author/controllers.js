67cd1ff8afaad9ebe95f6f4e994a9605b912e798
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-02-16 13:11:09 +0000

    added models author

--- src/author/controllers.js
+++ src/author/controllers.js
@@ -1,17 +1,16 @@
 const Author = require("./models");
 const Book = require("../books/model");
+
 const addAuthor = async (req, res) => {
   try {
     const author = await Author.create({
       authorName: req.body.authorName,
       authorId: req.body.authorId,
     });
-    res
-      .status(201)
-      .json({
-        message: `${author.title} Author was added successfully`,
-        author,
-      });
+    res.status(201).json({
+      message: `${author.title} Author was added successfully`,
+      author,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
---
+++ src/author/models.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db/connection");
+const Author = sequelize.define(
+  "Author",
+  { authorName: { type: DataTypes.STRING, allowNull: false } },
+  { timestamps: false }
+);
+module.exports = Author;
